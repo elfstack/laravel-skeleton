47eeb9ab8566233cc717f7f4f392b9923c148ec4
Stanley Cao <daydream2013@live.com>
2020-05-18 16:58:22 +0800

    fix: cannot logout user after unauthenticated response

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -14,34 +14,12 @@ Vue.use(AntDesign)
 Vue.use(VueMoment)
 Vue.component('listing', Listing)
 
+
 const app = new Vue({
     el: '#app',
     template: "<router-view />",
     store,
     router
-})
-
-const action = _.debounce(function (status) {
-    switch (status) {
-        case 401:
-            if (router.currentRoute.name !== 'Login') {
-                app.$message.warning('Logged out')
-                router.push({ name: 'login' });
-            }
-            break;
-        case 403:
-            app.$message.error('You don\'t have permission to do this')
-            break;
-        case 500:
-            app.$message.error('Server error')
-            break;
-    }
-}, 200)
-
-window.axios.interceptors.response.use(undefined, error => {
-    action(error.response.status)
-
-    return Promise.reject(error)
 })
 
 Vue.prototype.$can = value => {
--- resources/js/admin/bootstrap.js
+++ resources/js/admin/bootstrap.js
@@ -1,19 +1,49 @@
 import router from './routes'
+import axios from 'axios'
+import { debounce } from "lodash";
+import { message } from "ant-design-vue";
+import Vue from 'vue'
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-const axios = require('axios');
-
-window.axios = axios.create({
+const instance = axios.create({
     baseURL: '/admin/api',
     withCredentials: true,
     headers: {
         common: {
             'X-Requested-With': 'XMLHttpRequest'
         }
-    }
+    },
 })
+
+const action = debounce(function action (status) {
+    switch (status) {
+        case 401:
+            if (router.currentRoute.name !== 'Login') {
+                message.warning('Logged out')
+                router.push({ name: 'Login' });
+            }
+            break;
+        case 403:
+            message.error('You don\'t have permission to do this')
+            break;
+        case 500:
+            message.error('Server error')
+            break;
+    }
+}, 200)
+
+instance.interceptors.response.use(undefined, error => {
+    action(error.response.status)
+    return Promise.reject(error)
+})
+
+window.axios = instance
+Vue.prototype.axios = axios
+
+export default instance
 
